8f6d21d94b610a861777aba1a8fbe3528f33f03c
berilink <51522861+berilink@users.noreply.github.com>
2019-06-26 14:22:40 +0700

    Update berilink.js

--- berilink.js
+++ berilink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/10/the-top-10-technology-trends-of-2018.html"}
 if(x=="2"){xxx="/2018/09/the-best-free-games-offline-games-for.html"}
 if(x=="3"){xxx="/2018/09/how-to-create-bootable-usb-for-windows.html"}
 if(x=="4"){xxx="/2018/09/what-is-big-data.html"}
-if(x=="5"){xxx="/2018/09/what-is-blockchain.html"}a_to_ve[i].href="https://www.ruangnesia.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/09/what-is-blockchain.html"}a_to_ve[i].href="https://berilink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
